5faf787daf4ee6bfd00ba42ae2210457740afe50
Khelsea Mourinho Sihotang <162036813+khelseamourinho@users.noreply.github.com>
2024-10-02 05:55:41 +0700

    Change emoji

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = "You will be my girl˚❤˚".split("");
+    const titles = "You will be my girl❤️❤️".split("");
     const titleElement = document.getElementById("title");
     let index = 0;
 
